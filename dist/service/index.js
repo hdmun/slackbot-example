819e7af159851e92785230237dd3cd7c39c91e37
hdmun <huidae.mun@gmail.com>
2021-09-20 11:03:51 +0900

    딸려들어간 dist 폴더 삭제, gitignore 세팅

--- dist/service/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SlackService = void 0;
-var slack_1 = require("./slack");
-Object.defineProperty(exports, "SlackService", { enumerable: true, get: function () { return slack_1.SlackService; } });

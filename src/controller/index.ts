ad15e55f0ca710912e0fa0d0437aeccf36029ec3
hdmun <huidae.mun@gmail.com>
2021-09-24 20:42:39 +0900

    `url_verification` 타입 포맷 확인을 위해 로그 출력

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -9,16 +9,17 @@ const router = new Router()
 
 router.post('/slack/events', async (ctx: Context) => {
     const body = ctx.request.body
-    const event = body.event
 
     switch (body.type) {
         case 'event_callback':
             const slackService = Container.get(SlackService)
-            await slackService.event_callback(event)
+            await slackService.event_callback(body.event)
             ctx.status = 200
             break
         case 'url_verification':
-            console.log('url verification')  // Enable Events 등록할 때 필요하다
+            // Enable Events  등록할 때 필요하다
+            console.log('url_verification')
+            console.log(body)
             ctx.body = body.challenge
             break
         default:
